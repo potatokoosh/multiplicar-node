7dd3266220ea005fa551841be08156f77c838bd5
Jose Balcazar <potatokoosh@gmail.com>
2020-04-24 01:46:40 -0500

    + Comentarios

--- 02-app.js
+++ 02-app.js
@@ -3,7 +3,7 @@
 const {crearArchivo} = require('./multiplicar/multiplicar.js');
 
 
-let base = 's';
+let base = '4';
 
 crearArchivo(base)
   .then(archivo => console.log(`Archivo creado: ${archivo}`))
--- multiplicar/multiplicar.js
+++ multiplicar/multiplicar.js
@@ -12,20 +12,21 @@ const fs = require('fs');// obligatorio para poder usar fs.writeFile
           let data ='';//pongo un string vacio por que con el for le doy los valores
 
           for (let i = 1; i <= 10 ; i++){
-            data +=`${base} * ${i} = ${base * i}\n`;
+            data +=`${base} * ${i} = ${base * i}\n`;// el \n es un enter
+            // el += lo que hace es que guarda todos los resultado en la variable data, si no puciera el + y solo pongo =, lo que haria es que solo guarda la ultima operacion en la variable data ,,,y la menciono arriba del for como una variable vacia primero.
           }
 
           //const data = new Uint8Array(Buffer.from('Hello Node.js'));
           //el primer parametro es el nombre del archivo con el que lo va a crear, y la ubicacion si quiero guardarlo en un lugar diferente
           // el segundo parametro es lo que va a tener por dentro el archivo
           // y el tercer parametro es el error
-          //al poner el nombre del archivo con `${}`, lo que hace es que si vuelve dinamica y si cambio el valor de la vase se guarda como un archivo nuevo y no reescribe porque el archivo cambia de nombre
+          //al poner el nombre del archivo con `${}`, lo que hace es que si vuelve dinamica y si cambio el valor de la base se guarda como un archivo nuevo y no reescribe porque el archivo cambia de nombre
           fs.writeFile(`tablas/table-${base}.txt`, data, (err) => {
 
             if (err) 
                 reject (err)
             else 
-                resolve(`tabla-${base}.txt`)
+                resolve(`tabla-${base}.txt`)// este es el mensaje que sale en consola cuando no hay un error, adicinal de resolver el         fs.writeFile
             
           });
 
